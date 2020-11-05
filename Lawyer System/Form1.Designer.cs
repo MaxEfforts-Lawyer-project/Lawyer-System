1970b370df17817f110c943c8d851fc577a9509a
Mena Samer <50605289+MenaSamer285@users.noreply.github.com>
2020-11-05 17:07:32 +0200

    asdasdasdsad

--- Lawyer System/Form1.Designer.cs
+++ Lawyer System/Form1.Designer.cs
@@ -311,7 +311,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(390, 74);
+            this.comboBox1.Location = new System.Drawing.Point(413, 133);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(250, 24);
             this.comboBox1.TabIndex = 0;
